62cbeb3f95fe8c5bcd0b7ae1b1e47d68b1f58599
kalab-abebe <kalabdemelash1234@gmail.com>
2026-09-22 19:42:35 -0500

    Init commit

---
+++ src/Evenodd.java
@@ -0,0 +1,7 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int numToExamine = 7;
+        int remainder = numToExamine % 2;
+        System.out.println("The number " + numToExamine + " modulo 2 is " + remainder);
+    }
+}
